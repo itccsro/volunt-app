85630bd5b21bc5c066b196e00fe136f1a8755e1e
Remus Rusanu <hi@cezarneaga.eu>
2017-07-18 11:10:46 +0000

    remove new_applicant_path

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -6,17 +6,17 @@ class RedirectController < ApplicationController
   def me
     profile = Profile.for_email(current_user_email)
     if profile.nil?
-      redirect_to new_applicant_path
+      logout_user
+      redirect_to home_path
     elsif profile.is_coordinator?
       redirect_to coordinator_path(profile)
     elsif profile.is_fellow?
       redirect_to fellow_path(profile)
     elsif profile.is_volunteer?
       redirect_to volunteer_path(profile)
-    elsif profile.is_applicant?
-      redirect_to applicant_path(profile)
     else
-      redirect_to new_applicant_path
+      logout_user
+      redirect_to home_path
     end
   end
 end
