bfd4870df2bc66b8a1b5f3ff519521b742aace3f
Catalin <catalin@digitallysolid.com>
2017-07-27 22:38:44 +0100

    Fix flags for John Doe in seed data
    0x1 & 0x2 & 0x4 & 0x8 will result in 0x0, | should have been used
    instead, replaced the result with the desired one (0xF) and
    explained in the comment what that means

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ case Rails.env
 when "development"
   # env == "development":
   profile1 = Profile.create!(:full_name => "John Doe", :nick_name => "johndoe", :email => "johndoe@civictech.ro",
-                             :flags => 0x1 & 0x2 & 0x4 & 0x8)
+                             :flags => 0xF) # (0x1 | 0x2 | 0x4 | 0x8; all flags)
   profile2 = Profile.create!(:full_name => "Coordinator", :nick_name => "coordinator",
                              :email => "coordinator@civictech.ro", :flags => 0x8)
   profile3 = Profile.create!(:full_name => "Fellow", :nick_name => "fellow", :email => "fellow@civictech.ro",
